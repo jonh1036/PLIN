aa52256a9ab5f6f78518bdcbd687859b5622f3af
Jonh1036 <jonh1036@hotmail.com>
2019-01-02 16:17:03 -0300

    Cirando a estrutura dos casos de teste

--- src/Produtor.java
+++ src/Produtor.java
@@ -2,12 +2,28 @@ import java.util.Scanner;
 public class Produtor {
 	public static void main(String[] args) {
 		int meses[] = new int[12];
+		int valorMinimo[] = new int[12]; 
+		int leiteFaltante, leiteArmazenado;
+		final int producaoNormal = 900, custoNormal = 10;
+		
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("=====DEMANDAS DO MÊS=====");
+		System.out.println("===== DEMANDAS DO MÊS =====");
 		for(int i=0; i<12; i++) {
 			System.out.println("Mês "+(i+1)+":");
 			meses[i] = sc.nextInt();
+		}
+		
+		for(int i=0; i<12; i++) {
+			if(meses[i]>900) {
+				
+			}
+			else if(meses[i] == 900) {
+				
+			}
+			else {
+				
+			}
 		}
 		
 	}
