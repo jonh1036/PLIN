073d7a2bc0ad79b48a19c1f70c5b5e6b6c4386f2
Jonh1036 <jonh1036@hotmail.com>
2019-01-02 15:52:56 -0300

    Pedindo dados

--- src/Produtor.java
+++ src/Produtor.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
 public class Produtor {
 	public static void main(String[] args) {
-
+		int meses[] = new int[12];
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("=====DEMANDAS DO MÊS=====");
+		for(int i=0; i<12; i++) {
+			System.out.println("Mês "+(i+1)+":");
+			meses[i] = sc.nextInt();
+		}
+		
 	}
 }
