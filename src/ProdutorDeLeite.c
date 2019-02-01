8df2736335855aa35b8bb2c8f361449eeea1a0ec
Brena Deusyane <deusyane.1000@gmail.com>
2019-01-31 22:58:51 -0300

    Add files via upload

--- src/ProdutorDeLeite.c
+++ src/ProdutorDeLeite.c
@@ -1,50 +1,87 @@
+//Produdor de Leite
 #include <stdio.h>
+#include <stdbool.h>
 
-void comExtra(int litros_lic);
-void semExtra(int litros_lic);
-double arm, custo;
-int qnt_arm;
+int main(void){
+  int demanda[12], producaoAtual[12], horaExtra[12], armazenamento[12], d[12], n = 3;
+  float custo[12];
 
-int main(void) {
-  int litros_lic, op = 1;
+  for(int i = 0; i<n; i++){
+    printf("\nInsira a demanda de leite do %d° mês:  ", i+1);
+    scanf("%d", & demanda[i]);
+    d[i] = demanda[i];
+  }
+  
+  for(int i = 0; i<n; i++){
 
-  do{
-      printf("\nQuantos litros no %d° mês?\n",op);
-      scanf("%d",& litros_lic);
+    if(armazenamento[i-1] > 0 & armazenamento[i-1] < 1100){
+      demanda[i] = demanda[i] - armazenamento[i-1];
+    }
 
-    if(qnt_arm > 0){
-      if(qnt_arm < litros_lic){
-        litros_lic = litros_lic - qnt_arm;
+    if(demanda[i] == 1100){
+      armazenamento[i] = 0;
+      producaoAtual[i] = demanda[i];
+      horaExtra[i] = (producaoAtual[i] - 900)/100;
+    }
+    else if(demanda[i] <= 900){
+      if(demanda[i+1] > 1100){
+        if( (demanda[i+1] - 1100) <= 200){
+          armazenamento[i] = demanda[i+1] - 1100;
+          producaoAtual[i] = demanda[i] + armazenamento[i];
+          horaExtra[i] = (producaoAtual[i] - 900)/100;
+        }
+        else{
+          armazenamento[i] = 0;
+          producaoAtual[i] = demanda[i];
+          horaExtra[i] = 0;
+        }
       }
-      else
-        litros_lic = qnt_arm - litros_lic; 
+      else{
+        armazenamento[i] = 0;
+        producaoAtual[i] = demanda[i];
+        horaExtra[i] = 0;
+      }      
+    }
+    else if(demanda[i] > 900 & demanda[i] < 1100){
+      if(demanda[i+1] > 1100){
+        if( (demanda[i] + (demanda[i+1] - 1100) <= 1100)){
+          armazenamento[i] = demanda[i+1] - 1100;
+          producaoAtual[i] = demanda[i] + armazenamento[i];
+          horaExtra[i] = (producaoAtual[i] - 900)/100;
+        }
+        else{
+          armazenamento[i] = 0;
+          producaoAtual[i] = demanda[i];
+          horaExtra[i] = (producaoAtual[i] - 900)/100;          
+        }
+      }
+      else{
+        armazenamento[i] = 0;
+        producaoAtual[i] = demanda[i];
+        horaExtra[i] = (producaoAtual[i] - 900)/100;
+      }
+    }
+    else{
+      producaoAtual[i] = armazenamento[i] = horaExtra[i] = 0;
     }
 
-    if(litros_lic <= 900){
-      semExtra(litros_lic);
-      printf("\nO menor valor a ser cobrado sem ter prejuizo no %d° mês é: \nR$ %.2lf\n", op, custo);
+    if(producaoAtual[i] > 900){
+      producaoAtual[i] = 900;
     }
-    else if(litros_lic > 900 && litros_lic <= 1100){
-      comExtra(litros_lic);
-      printf("\nO menor valor a ser cobrado sem ter prejuizo no %d° mês é: \nR$ %.2lf\n", op, custo);
+
+    custo[i] = producaoAtual[i] * 0.1 + horaExtra[i] * 13 + armazenamento[i] * 0.015;
+  }
+
+  for(int i = 0; i<n; i++){
+    if(custo[i]>0){
+      printf("\nA demanda do %d° mês de %dL custará: R$ %.2f", i+1, d[i], custo[i]);
     }
-    else
-      printf("\nA demanda é maior que a produção mensal!\n");
+    else{
+      printf("\nNo mês %d° não será possível produzir %dL de leite!", i+1, d[i]);
+    }   
+  }
 
-    op++;
-    qnt_arm = arm;
-
-  }while(op < 13);
-
-  return 1;
-}
-void comExtra(int litros_lic){  
-  arm = 1100 - litros_lic;
-  arm = arm * 0.015;
-  custo = 143 + arm;
-}
-void semExtra(int litros_lic){
-  arm = 900 - litros_lic;
-  arm = arm * 0.015;
-  custo = 90 + arm;
+  for(int i = 0; i<n; i++){
+    printf("\n %d° \nArmazenamento: %d\nHora Extra: %d\nProdução Sem Extra: %d\n",i+1, armazenamento[i], horaExtra[i], producaoAtual[i]);
+  }
 }
